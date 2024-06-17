51022b306effd3317860f6109f28d10bb3db4cfb
federicosoler <49198980+federicosoler@users.noreply.github.com>
2024-06-16 23:18:01 -0300

    Excepciones para el Main

--- src/AdministradorProyectos/Empleado/MainEmpleado.java
+++ src/AdministradorProyectos/Empleado/MainEmpleado.java
@@ -1,18 +1,20 @@
 package AdministradorProyectos.Empleado;
 
+import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
+
+import AdministradorProyectos.Exceptions.DAOException;
 
 public class MainEmpleado {
 	public static void main(String[] args) {
-		EmpleadoDAO empleadoDAO = new EmpleadoH2Impl();
-
-		EmpleadoService empleadoService = new EmpleadoService(empleadoDAO);
-
-		SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				EmpleadoUI frame = new EmpleadoUI(empleadoService);
-				frame.setVisible(true);
-			}
-		});
+	    try {
+	        EmpleadoDAO empleadoDAO = new EmpleadoH2Impl();
+	        EmpleadoService empleadoService = new EmpleadoService(empleadoDAO);
+	        EmpleadoUI frame = new EmpleadoUI(empleadoService);
+	        frame.setVisible(true);
+	    } catch (DAOException e) {
+	        JOptionPane.showMessageDialog(null, "Error al conectar con la base de datos: " + e.getMessage());
+	        System.exit(1);
+	    }
 	}
 }
