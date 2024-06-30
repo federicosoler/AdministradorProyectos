0f5391076b8b69ed9a179d014bb99ee6e9daaa84
federicosoler <49198980+federicosoler@users.noreply.github.com>
2024-06-30 01:12:12 -0300

    Eliminar MainEmpleado y crear clase Main en su propio paquete Main.

--- src/AdministradorProyectos/Main/Main.java
+++ src/AdministradorProyectos/Main/Main.java
@@ -1,10 +1,22 @@
 package AdministradorProyectos.Main;
 
+import AdministradorProyectos.Empleado.EmpleadoDAO;
+import AdministradorProyectos.Empleado.EmpleadoH2Impl;
+import AdministradorProyectos.Empleado.EmpleadoService;
+import AdministradorProyectos.Empleado.EmpleadoUI;
+import AdministradorProyectos.Exceptions.DAOException;
+import javax.swing.JOptionPane;
+
 public class Main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+    public static void main(String[] args) {
+        try {
+            EmpleadoDAO empleadoDAO = new EmpleadoH2Impl();
+            EmpleadoService empleadoService = new EmpleadoService(empleadoDAO);
+            EmpleadoUI frame = new EmpleadoUI(empleadoService);
+            frame.setVisible(true);
+        } catch (DAOException e) {
+            JOptionPane.showMessageDialog(null, "Error al conectar con la base de datos: " + e.getMessage());
+            System.exit(1);
+        }
+    }
 }
