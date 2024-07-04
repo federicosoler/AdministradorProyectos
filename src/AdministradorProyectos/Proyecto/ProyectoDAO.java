884f0cfbb95283883b26d4d59ebb8c644172c8d2
federicosoler <49198980+federicosoler@users.noreply.github.com>
2024-07-04 04:38:25 -0300

    Tarea

--- src/AdministradorProyectos/Proyecto/ProyectoDAO.java
+++ src/AdministradorProyectos/Proyecto/ProyectoDAO.java
@@ -1,17 +1,16 @@
 package AdministradorProyectos.Proyecto;
 
+import AdministradorProyectos.Empleado.Empleado;
 import AdministradorProyectos.Exceptions.DAOException;
+import AdministradorProyectos.Tarea.Tarea;
 
 import java.util.List;
 
 public interface ProyectoDAO {
-	void guardarProyecto(Proyecto proyecto) throws DAOException;
-	
-	List<Proyecto> obtenerTodosLosProyectos() throws DAOException;
-
-	void actualizarProyecto(Proyecto proyecto) throws DAOException;
-
-	void eliminarProyecto(String nombre) throws DAOException;
-
-	void asignarEmpleadoAProyecto(String nombreProyecto, String nombreEmpleado) throws DAOException;
+    void guardarProyecto(Proyecto proyecto) throws DAOException;
+    Proyecto obtenerProyectoPorNombre(String nombre) throws DAOException;
+    List<Proyecto> obtenerTodosLosProyectos() throws DAOException;
+    void eliminarProyecto(String nombre) throws DAOException;
+    void actualizarProyecto(Proyecto proyecto) throws DAOException;
+    void asignarEmpleadoAProyecto(String proyectoNombre, Empleado empleado) throws DAOException;
 }
--- src/AdministradorProyectos/Proyecto/ProyectoService.java
+++ src/AdministradorProyectos/Proyecto/ProyectoService.java
@@ -1,7 +1,8 @@
 package AdministradorProyectos.Proyecto;
 
-import AdministradorProyectos.Exceptions.ServiceException;
+import AdministradorProyectos.Empleado.Empleado;
 import AdministradorProyectos.Exceptions.DAOException;
+import AdministradorProyectos.Exceptions.ServiceException;
 
 import java.util.List;
 
@@ -20,6 +21,14 @@ public class ProyectoService {
         }
     }
 
+    public Proyecto obtenerProyectoPorNombre(String nombre) throws ServiceException {
+        try {
+            return proyectoDAO.obtenerProyectoPorNombre(nombre);
+        } catch (DAOException e) {
+            throw new ServiceException("Error al obtener el proyecto", e);
+        }
+    }
+
     public List<Proyecto> obtenerTodosLosProyectos() throws ServiceException {
         try {
             return proyectoDAO.obtenerTodosLosProyectos();
@@ -33,6 +42,22 @@ public class ProyectoService {
             proyectoDAO.eliminarProyecto(nombre);
         } catch (DAOException e) {
             throw new ServiceException("Error al eliminar el proyecto", e);
+        }
+    }
+
+    public void actualizarProyecto(Proyecto proyecto) throws ServiceException {
+        try {
+            proyectoDAO.actualizarProyecto(proyecto);
+        } catch (DAOException e) {
+            throw new ServiceException("Error al actualizar el proyecto", e);
+        }
+    }
+
+    public void asignarEmpleadoAProyecto(String proyectoNombre, Empleado empleado) throws ServiceException {
+        try {
+            proyectoDAO.asignarEmpleadoAProyecto(proyectoNombre, empleado);
+        } catch (DAOException e) {
+            throw new ServiceException("Error al asignar el empleado al proyecto", e);
         }
     }
 }
